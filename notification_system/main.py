1450171a9f58510d7614468222937c7133299118
Archie <152638120+Gamez99@users.noreply.github.com>
2025-03-28 12:00:35 -0600

    Delete notification_system directory

--- notification_system/main.py
+++
@@ -1,6 +0,0 @@
-from win11toast import toast
-
-def send_notification(text: str, *, title: str = '', icon: str = '', on_click: str = ''):
-    toast(title, text, on_click=on_click, icon=icon)
-
-send_notification("Hello", title="Python Test", on_click='https://pypi.org/project/win11toast/')
